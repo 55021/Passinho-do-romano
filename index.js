6038f49b12a3926da051fa2ee993358ea8ee89e2
Maria Rita <msantana@id.uff.br>
2022-01-21 15:43:55 -0300

    👀 quase lá

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-let encode = document.querySelector('.botaoC');
-let decode = document.querySelector('.botaoD')
+let encode = document.querySelector('#botaoC');
+let decode = document.querySelector('#botaoD');
 let output = document.querySelector('#resultadoaqui');
 let input = document.querySelector('#textoaqui');
+let option = document.querySelector('select');
+let inc = document.querySelector('.incremento');
+
+// DEFINIR N!!!!!!!!!!!
+
+option.addEventListener('change', () => {
+    if (option.value == "base64") {
+        inc.style.display = "none";
+    } else if (option.value == "cifraDeCesar") {
+        inc.style.display = "block";    
+    }
+})
 
 encode.addEventListener('click', () => {
-    output.value = btoa(input.value);
+    if (option.value == "base64") {
+        output.value = btoa(input.value);
+    } else if (option.value == "cifraDeCesar") {
+        
+    }
 })
 
 decode.addEventListener('click', () => {
     /* var string64 = btoa(input.value);
     var decode1 = atob(string64); */ // esse aqui serviria se eu quisesse decodificar de volta a caixa de texto do resultado
-    var decode1 = atob(input.value);
-    output.value = decode1;
+    if (option.value == "base64") {
+        var decode1 = atob(input.value);
+        output.value = decode1;
+    } else if (document.querySelector('#cifraDeCesar').checked == true) {
+        
+    }
 })
+
+/* Dúvidas pelo caminho:
+P: Como eu eu boto esse addEventListener dentro de um if?????
+R: Não, mas dá pra colocar os ifs dentro do addEventListener.
+
+Coisas que eu aprendi mas ainda não entendi totalmente:
+Por que eu não posso declarar com o .value e sim declarar a variável e depois usar ela com o .value?*/
