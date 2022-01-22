d0816085783c5f961f4fa3deaef781fc3a3782a3
Maria Rita <msantana@id.uff.br>
2022-01-22 00:12:57 -0300

    ✅ up and running!

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-let encode = document.querySelector('#botaoC');
-let decode = document.querySelector('#botaoD');
+// TEXTAREAS
 let output = document.querySelector('#resultadoaqui');
 let input = document.querySelector('#textoaqui');
+// DROPDOWN MENU
 let option = document.querySelector('select');
-let inc = document.querySelector('.incremento');
+// SHIFT CIFRA DE CÉSAR
+let inc = document.querySelector('.inputincremento');
+const inputIncremento = document.querySelector('#incremento');
+// OPÇÕES INPUT RADIO
+let encode = document.querySelector('#codificar');
+let decode = document.querySelector('#decodificar');
+// BOTÃO FINAL
+let oBotao = document.querySelector('.botao001');
 
-// DEFINIR N!!!!!!!!!!!
-
+// Input do incremento aparecendo ou não
 option.addEventListener('change', () => {
     if (option.value == "base64") {
         inc.style.display = "none";
@@ -15,28 +21,64 @@ option.addEventListener('change', () => {
     }
 })
 
+// Trocar o texto do botão
 encode.addEventListener('click', () => {
-    if (option.value == "base64") {
-        output.value = btoa(input.value);
-    } else if (option.value == "cifraDeCesar") {
-        
-    }
+    oBotao.innerText = "Codificar mensagem!";
 })
 
 decode.addEventListener('click', () => {
-    /* var string64 = btoa(input.value);
-    var decode1 = atob(string64); */ // esse aqui serviria se eu quisesse decodificar de volta a caixa de texto do resultado
-    if (option.value == "base64") {
-        var decode1 = atob(input.value);
-        output.value = decode1;
-    } else if (document.querySelector('#cifraDeCesar').checked == true) {
-        
-    }
+    oBotao.innerText = "Decodificar mensagem!"
 })
 
-/* Dúvidas pelo caminho:
-P: Como eu eu boto esse addEventListener dentro de um if?????
-R: Não, mas dá pra colocar os ifs dentro do addEventListener.
+function cifraCesar1(mensagemDigitada) {
+    var resultado = '';
+    var incremento = +inputIncremento.value;
 
-Coisas que eu aprendi mas ainda não entendi totalmente:
-Por que eu não posso declarar com o .value e sim declarar a variável e depois usar ela com o .value?*/
+    for (var i = 0; i < mensagemDigitada.length; i++) {
+        if (mensagemDigitada[i].charCodeAt() >= 65 && mensagemDigitada[i].charCodeAt() <= 90) {             
+            resultado += String.fromCharCode(((mensagemDigitada[i].charCodeAt() - 65 + incremento) % 26) + 65);
+        }
+        else if (mensagemDigitada[i].charCodeAt() >= 97 && mensagemDigitada[i].charCodeAt() <= 122) {
+            resultado += String.fromCharCode(((mensagemDigitada[i].charCodeAt() - 97 + incremento) % 26) + 97);
+            console.log(String.fromCharCode(((mensagemDigitada[i].charCodeAt() - 97 + incremento) % 26) + 97));
+        }
+        else {
+            resultado += mensagemDigitada[i];
+        }
+    }
+    return resultado;
+}
+
+function cifraCesar2(mensagemDigitada) {
+    var resultado = '';
+    var incremento = +inputIncremento.value;
+
+    for (var i = 0; i < mensagemDigitada.length; i++) {
+        if (mensagemDigitada[i].charCodeAt() >= 65 && mensagemDigitada[i].charCodeAt() <= 90) {
+            
+            resultado += String.fromCharCode(((mensagemDigitada[i].charCodeAt() - 90 - incremento) % 26) + 90);
+        }
+        else if (mensagemDigitada[i].charCodeAt() >= 97 && mensagemDigitada[i].charCodeAt() <= 122) {
+            resultado += String.fromCharCode(((mensagemDigitada[i].charCodeAt() - 122 - incremento) % 26) + 122);
+        }
+        else {
+            resultado += mensagemDigitada[i];
+        }
+    }
+    
+    return resultado;
+}
+
+// Parte final: codificar ou decodificar
+// Escrever no placeholder pro input não incluir acentos. nem número
+oBotao.addEventListener('click', () => {
+    if (encode.checked == true && option.value == "base64") {
+        output.value = btoa(input.value);
+    } else if (decode.checked == true && option.value == "base64") {
+        output.value = atob(input.value);
+    } else if (encode.checked == true && option.value == "cifraDeCesar") {
+        output.value = cifraCesar1(input.value);
+    } else if (decode.checked == true && option.value == "cifraDeCesar") {
+        output.value = cifraCesar2(input.value);
+    }
+})
